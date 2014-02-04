697f6de5bae9413d1b38e3857e4d1f810ee42c8d
Callum Smith <callum1234moo@gmail.com>
2014-02-04 00:12:28 -0500

    NOW WITH 100% MORE SAIL

--- sail.js
+++ sail.js
@@ -1,4 +1,9 @@
-var SAIL = document.getElementById("SAIL");
+var SAIL = document.getElementById("SAIL"),
+COUNTSAIL = 0,
+HOWSAIL = document.getElementById("COUNTSAIL");
 function SAILING(){
+	SAIL = new Audio("sail.mp3");
 	SAIL.play();
+	COUNTSAIL ++;
+	HOWSAIL.textContent = COUNTSAIL;
 }
