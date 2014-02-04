3f11791e112f50f20c3837c6609317b6aea1f591
Callum Smith <callum1234moo@gmail.com>
2014-02-04 12:09:08 -0500

    NOW WITH CHIVES

--- sail.js
+++ sail.js
@@ -1,5 +1,7 @@
 var SAIL = document.getElementById("SAIL"),
 
+TWENTY = document.getElementById("TWENTY"),
+
 COUNTSAIL = 0,
 
 HOWSAIL = document.getElementById("COUNTSAIL"),
@@ -17,16 +19,43 @@ SAILING = {
 		COUNTSAIL ++;
 		HOWSAIL.textContent = COUNTSAIL;
 		CONSAIL.LOG(COUNTSAIL);
+		if (COUNTSAIL == 10) {
+			BACKGROUND.CHANGE("ocean.jpg");
+			ACHIVE.MENT(" SAIL SO HARD");
+		}
+		if (COUNTSAIL == 20){
+			TWENTY = new Audio("fullsail.mp3");
+			TWENTY.play();
+			ACHIVE.MENT(" FULLSAIL AHEAD");
+		}
+		if (COUNTSAIL == 30){
+			TWENTY.pause();
+			ACHIVE.MENT(" WOW SUCH SAIL");
+			BACKGROUND.CHANGE(" ");
+		}
+		if (COUNTSAIL == 50){
+			ACHIVE.MENT(" YOU ARE STILL SAIL?");
+		}
+		if (COUNTSAIL == 100) {
+			ACHIVE.MENT(" SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL");
+		}
 	}
 },
 
 BACKGROUND = {
 	CHANGE: function(file){
-		//css.setbackground(file);
+		document.body.style.background = '#fff url('+file+')';
 	}
-};
+},
 
-if (COUNTSAIL = 10) {
-	BACKGROUND.CHANGE("ocean.jpg");
-}
+ACHIVE = {
+	MENT: function(GET){
+		function HIDE(){
+			document.getElementById("ACHIVE").classList.add('HIDE');
+		}
+		document.getElementById("CHIVE").textContent = GET;
+		document.getElementById("ACHIVE").classList.remove('HIDE');
+		setTimeout(HIDE, 1500);
+	} 
+};
 
