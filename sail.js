727b1a13a8b7ad2713e023eefa9908209d7f0f2e
Callum Smith <callum1234moo@gmail.com>
2014-02-04 00:32:08 -0500

    MOVED SOME STUFF AROUND
    TYPE SAILING.AUDIO(); IN THE CONSOLE

--- sail.js
+++ sail.js
@@ -1,9 +1,32 @@
 var SAIL = document.getElementById("SAIL"),
+
 COUNTSAIL = 0,
-HOWSAIL = document.getElementById("COUNTSAIL");
-function SAILING(){
-	SAIL = new Audio("sail.mp3");
-	SAIL.play();
-	COUNTSAIL ++;
-	HOWSAIL.textContent = COUNTSAIL;
+
+HOWSAIL = document.getElementById("COUNTSAIL"),
+
+CONSAIL = { //LETS RENAME console.log SHALL WE?
+	LOG: function(stuff){
+		console.log(stuff);
+	}
+},
+
+SAILING = {
+		AUDIO: function(){
+		SAIL = new Audio("sail.mp3");
+		SAIL.play();
+		COUNTSAIL ++;
+		HOWSAIL.textContent = COUNTSAIL;
+		CONSAIL.LOG(COUNTSAIL);
+	}
+},
+
+BACKGROUND = {
+	CHANGE: function(file){
+		//css.setbackground(file);
+	}
+};
+
+if (COUNTSAIL = 10) {
+	BACKGROUND.CHANGE("ocean.jpg");
 }
+
