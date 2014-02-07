eb93a24dd71265c7bc2239f4b17be49d3829d4bc
Callum Smith <callum1234moo@gmail.com>
2014-02-06 23:08:27 -0500

    ADDED MORE CHIVES, AND MORE POINTLESS THINGS
    Next revision will feature per browser achivement logging
    functionality(cookies). I don't want to hook this up to a database.
    ever. Super chives are coming, as is the SAIL store: a store to "spend"
    the points you have on TOATLLY real things.

--- sail.js
+++ sail.js
@@ -6,12 +6,6 @@ COUNTSAIL = 0,
 
 HOWSAIL = document.getElementById("COUNTSAIL"),
 
-CONSAIL = { //LETS RENAME console.log SHALL WE?
-	LOG: function(stuff){
-		console.log(stuff);
-	}
-},
-
 ACHIVES = { '10': 'BACKGROUND.CHANGE("ocean.jpg"); ACHIVE.MENT(" SAIL SO HARD");',
 			'20': 'TWENTY = new Audio("fullsail.mp3"); TWENTY.play(); ACHIVE.MENT(" FULLSAIL AHEAD");',
 			'30': 'TWENTY.pause(); ACHIVE.MENT(" WOW SUCH SAIL"); BACKGROUND.CHANGE(" ");',
@@ -19,17 +13,45 @@ ACHIVES = { '10': 'BACKGROUND.CHANGE("ocean.jpg"); ACHIVE.MENT(" SAIL SO HARD");
 			'80': 'ACHIVE.MENT(" BLAME IT ON MY ADD"); BACKGROUND.CHANGE("boat.jpg");',
 			'90': 'BACKGROUND.CHANGE(" ");',
 			'99': 'ACHIVE.MENT(" I GOT 99 SAILS");',
-			'100': 'ACHIVE.MENT(" SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL");'
-			'300': 'ACHIVE.MENT("YOU REALLY LIKE SAILS, DONTCHA?");'
+			'100': 'ACHIVE.MENT(" SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL SAIL");',
+			'150': 'ACHIVE.MENT(" APPLE, CAUSE WHY NOT?"); BACKGROUND.CHANGE("http://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg");',
+			'160': 'BACKGROUND.CHANGE(" ");',
+			'200': 'ACHIVE.MENT(" WOW");',
+			'205': 'ACHIVE.MENT(" SUCH CLICK);',
+			'210': 'ACHIVE.MENT(" MUCH SAIL");',
+			'300': 'ACHIVE.MENT("YOU REALLY LIKE SAILS, DONTCHA?");',
+			'NaN': 'ACHIVE.MENT(" NaNNaNNaNNaNNaN");'
+},
+
+CONSAIL = { //LETS RENAME console.log SHALL WE?
+	LOG: function(stuff){
+		console.log(stuff);
+	},
+	DEV: function(){
+		ACHIVE.MENT(" A WINNER IS YOU");
+		COUNTSAIL = 99999999;
+		SAILING.AUDIO();
+		TWENTY = new Audio("fullsail.mp3"); 
+		document.getElementById("LAWLAWLAWL").innerHTML = '<button id="GOSAIL" onclick="CONSAIL.WIN()">PLAYING ></button>';
+		TWENTY.play();
+	},
+	WIN: function(){
+		if(!TWENTY.paused){
+			TWENTY.pause();
+			document.getElementById("GOSAIL").textContent = "PAUSED =";
+		} else if (TWENTY.paused){
+			TWENTY.play();
+			document.getElementById("GOSAIL").textContent = "PLAYING >";
+		}
+	}
 },
 
 SAILING = {
 		AUDIO: function(){
 		SAIL = new Audio("sail.mp3");
-		SAIL.play();
+		//SAIL.play();
 		COUNTSAIL ++;
 		HOWSAIL.textContent = COUNTSAIL;
-		CONSAIL.LOG(COUNTSAIL);
 		if (ACHIVES[COUNTSAIL]){
 			eval(ACHIVES[COUNTSAIL]);
 		}
@@ -52,4 +74,3 @@ ACHIVE = {
 		setTimeout(HIDE, 1500);
 	} 
 };
-
